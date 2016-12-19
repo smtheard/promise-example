92d4f6e13c198dffe9755b4241843814c563e6d7
Stefan Theard <smtheard@gmail.com>
2016-12-18 20:49:28 -0600

    add a nested callback

--- promises.js
+++ promises.js
@@ -1,7 +1,7 @@
 // most of this example derived from https://www.promisejs.org/
-
-fs = require('fs');
-
+'use strict'
+var fs = require('fs');
+var Promise = require('promise');
 
 // read file without promise:
 var readFile = function (filename) {
@@ -23,16 +23,14 @@ var readFile = function(filename, callback) {
 readFile('text_file_1.txt', function(err, res) {console.log('within callback with resp: ' + res)});
 
 
+var promiseReadFile = Promise.denodeify(require('fs').readFile);
 
-var promiseReadFile = function(filename){
-  return new Promise(function (fulfill, reject){
-    readFile(filename, 'utf8').done(function (res){
-      try {
-        fulfill(res);
-      } catch (ex) {
-        reject(ex);
-      }
-    }, reject);
-  });
+var p_readFile = function(filename, callback) {
+  return promiseReadFile(filename, 'utf8')
+    .then(function(res) {console.log('in then with resp: ' + res)})
+    .nodeify(callback);
 }
 
+p_readFile('text_file_1.txt', 
+  p_readFile('text_file_2.txt', function(){console.log("nested callback after reading text file 2")})
+).done(function() {console.log("promise done")});
